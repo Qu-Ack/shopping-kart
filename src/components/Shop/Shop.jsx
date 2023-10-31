19790af45ba78a9d1b220231926df1d9c51dfbd5
Qu-Ack <134773611+Qu-Ack@users.noreply.github.com>
2023-10-31 21:56:37 +0530

    made our json server and added data in it. Fetched that data and displayed on mount of the shop component

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,7 +1,49 @@
+import { useEffect, useState } from "react"
+
 function Shop() {
+
+    const [data, setData] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function getData() {
+            try {
+                const tmp = await fetch("http://127.0.0.1:3000/products");
+                if (!tmp.ok) {
+                    throw new Error(`this is a HTTP error: the status is ${tmp.status}`)
+                }
+                const products = await tmp.json();
+                setData(products);
+                setError(null)
+            } catch(err) {
+                setError(err.message);
+            } finally {
+                setLoading(false)
+            }
+
+        }
+
+        getData();
+    } , []);
+
+    const items = data.map(element => {
+        return(
+            <div className="card" key={element.id}>
+                {element.name}
+            </div>
+        )
+    })
+    // console.log(items)
+
+    if (loading) return <h1>Loading ...</h1>
+    if (error) return <h1>We have encountered an error</h1>
+
     return(
-        <h1>Shop</h1>
-    )
+        <div>
+            {items}
+        </div>
+        )
 }
 
 export default Shop
